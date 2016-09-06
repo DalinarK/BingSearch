5a558e40ec0a9132d59bd9d63edd295b1f7f475b
Dustin <djdinh@gmail.com>
2016-09-06 14:48:33 -0700

    no message

---
+++ src/main/java/googleTopSearches/Searches.java
@@ -0,0 +1,159 @@
+package googleTopSearches;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by djdin on 9/4/2016.
+ */
+
+@Generated("org.jsonschema2pojo")
+public class Searches {
+
+    @SerializedName("42")
+    @Expose
+    public List<String> _42 = new ArrayList<String>();
+    @SerializedName("43")
+    @Expose
+    public List<String> _43 = new ArrayList<String>();
+    @SerializedName("49")
+    @Expose
+    public List<String> _49 = new ArrayList<String>();
+    @SerializedName("52")
+    @Expose
+    public List<String> _52 = new ArrayList<String>();
+    @SerializedName("24")
+    @Expose
+    public List<String> _24 = new ArrayList<String>();
+    @SerializedName("25")
+    @Expose
+    public List<String> _25 = new ArrayList<String>();
+    @SerializedName("26")
+    @Expose
+    public List<String> _26 = new ArrayList<String>();
+    @SerializedName("27")
+    @Expose
+    public List<String> _27 = new ArrayList<String>();
+    @SerializedName("21")
+    @Expose
+    public List<String> _21 = new ArrayList<String>();
+    @SerializedName("48")
+    @Expose
+    public List<String> _48 = new ArrayList<String>();
+    @SerializedName("23")
+    @Expose
+    public List<String> _23 = new ArrayList<String>();
+    @SerializedName("46")
+    @Expose
+    public List<String> _46 = new ArrayList<String>();
+    @SerializedName("47")
+    @Expose
+    public List<String> _47 = new ArrayList<String>();
+    @SerializedName("44")
+    @Expose
+    public List<String> _44 = new ArrayList<String>();
+    @SerializedName("45")
+    @Expose
+    public List<String> _45 = new ArrayList<String>();
+    @SerializedName("28")
+    @Expose
+    public List<String> _28 = new ArrayList<String>();
+    @SerializedName("29")
+    @Expose
+    public List<String> _29 = new ArrayList<String>();
+    @SerializedName("40")
+    @Expose
+    public List<String> _40 = new ArrayList<String>();
+    @SerializedName("41")
+    @Expose
+    public List<String> _41 = new ArrayList<String>();
+    @SerializedName("1")
+    @Expose
+    public List<String> _1 = new ArrayList<String>();
+    @SerializedName("3")
+    @Expose
+    public List<String> _3 = new ArrayList<String>();
+    @SerializedName("5")
+    @Expose
+    public List<String> _5 = new ArrayList<String>();
+    @SerializedName("4")
+    @Expose
+    public List<String> _4 = new ArrayList<String>();
+    @SerializedName("6")
+    @Expose
+    public List<String> _6 = new ArrayList<String>();
+    @SerializedName("9")
+    @Expose
+    public List<String> _9 = new ArrayList<String>();
+    @SerializedName("8")
+    @Expose
+    public List<String> _8 = new ArrayList<String>();
+    @SerializedName("51")
+    @Expose
+    public List<String> _51 = new ArrayList<String>();
+    @SerializedName("39")
+    @Expose
+    public List<String> _39 = new ArrayList<String>();
+    @SerializedName("38")
+    @Expose
+    public List<String> _38 = new ArrayList<String>();
+    @SerializedName("10")
+    @Expose
+    public List<String> _10 = new ArrayList<String>();
+    @SerializedName("13")
+    @Expose
+    public List<String> _13 = new ArrayList<String>();
+    @SerializedName("12")
+    @Expose
+    public List<String> _12 = new ArrayList<String>();
+    @SerializedName("15")
+    @Expose
+    public List<String> _15 = new ArrayList<String>();
+    @SerializedName("14")
+    @Expose
+    public List<String> _14 = new ArrayList<String>();
+    @SerializedName("17")
+    @Expose
+    public List<String> _17 = new ArrayList<String>();
+    @SerializedName("16")
+    @Expose
+    public List<String> _16 = new ArrayList<String>();
+    @SerializedName("19")
+    @Expose
+    public List<String> _19 = new ArrayList<String>();
+    @SerializedName("18")
+    @Expose
+    public List<String> _18 = new ArrayList<String>();
+    @SerializedName("31")
+    @Expose
+    public List<String> _31 = new ArrayList<String>();
+    @SerializedName("30")
+    @Expose
+    public List<String> _30 = new ArrayList<String>();
+    @SerializedName("37")
+    @Expose
+    public List<String> _37 = new ArrayList<String>();
+    @SerializedName("36")
+    @Expose
+    public List<String> _36 = new ArrayList<String>();
+    @SerializedName("35")
+    @Expose
+    public List<String> _35 = new ArrayList<String>();
+    @SerializedName("34")
+    @Expose
+    public List<String> _34 = new ArrayList<String>();
+    @SerializedName("33")
+    @Expose
+    public List<String> _33 = new ArrayList<String>();
+    @SerializedName("32")
+    @Expose
+    public List<String> _32 = new ArrayList<String>();
+    @SerializedName("50")
+    @Expose
+    public List<String> _50 = new ArrayList<String>();
+
+}
---
+++ src/main/java/googleTopSearches/topSearches.java
@@ -0,0 +1,33 @@
+package googleTopSearches;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Created by djdin on 9/6/2016.
+ */
+public class topSearches {
+
+    public Searches parseGoogle () {
+        String strTemp = "";
+        Searches searches = new Searches();
+        Gson gson = new Gson();
+        try {
+            URL url = new URL("http://hawttrends.appspot.com/api/terms/");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            while (null != (strTemp = br.readLine())) {
+//                System.out.println(strTemp);
+
+                searches = gson.fromJson(strTemp, Searches.class);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+//        System.out.println(gson.toJson(searches, Searches.class));
+        return searches;
+    }
+}
